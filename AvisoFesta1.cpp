c45eae7b7f7353dd6b9a28c070a93deb6e17bc4c
Gabriel <gaplogan@gmail.com>
2020-12-24 15:46:18 -0300

    2020.12.24.15.46

---
+++ AvisoFesta1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    cout << "Hoje vai ter festa na casa da Fabi" << endl;
+    cout << "Hoje vai ter festa na casa da Fabi" << endl;
+    cout << "Hoje vai ter festa na casa da Fabi" << endl;
+    cout << "Hoje vai ter festa na casa da Fabi" << endl;
+    cout << "Hoje vai ter festa na casa da Fabi" << endl;
+
+    return 0;
+}
